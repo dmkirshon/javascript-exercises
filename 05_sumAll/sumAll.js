606e0022afef84684ab5f6bcab515a2abea1995f
dmkirshon <kirshon.david@gmail.com>
2021-11-30 17:35:15 +0200

    feat: Implement sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if('number' != typeof(first) ||
+            'number' != typeof(second) ||
+            first < 0 || second < 0) return 'ERROR';
 
+    const start = first < second ? first : second;
+    const finish = start === second ? first: second;
+
+    let sum = 0;
+    
+    for(i = start; i <= finish; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
