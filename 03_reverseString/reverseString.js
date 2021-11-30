d0b2fe96f3bbb4a58401258d9ab4dcf353fe9174
dmkirshon <kirshon.david@gmail.com>
2021-11-30 13:32:41 +0200

    feat: Implement reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+    let reverseStr = '';
+    for(i = str.length-1; i >= 0; i-- ) {
+        reverseStr += str[i];
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
