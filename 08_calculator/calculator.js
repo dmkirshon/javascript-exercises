ad35e05f40239c2374d9681fa6df6c470d392ded
dmkirshon <kirshon.david@gmail.com>
2021-12-27 10:43:18 +0200

    feat: Implement calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(values) { 
+  // let total = 0;
+
+  // for (i = 0; i < values.length; i++) {
+  //   total += values[i];
+  // }
+  // return total;
+
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  // let total = 1;
 
+  // for (i = 0; i < values.length; i++) {
+  //   total *= values[i];
+  // }
+  // return total;
+
+   return array.length
+  ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+  : 0;
 };
 
-const power = function() {
-	
+const power = function(base,exp) {
+  total = 1;
+
+	while(exp > 0) {
+    total *= base;
+    exp--;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+	if(value === 0)
+    return 1;
+  return value * factorial(value - 1);
 };
 
 // Do not edit below this line
