a3259769200769636335f17b8020b2979aa51922
dmkirshon <kirshon.david@gmail.com>
2021-12-27 11:05:27 +0200

    feat: Implement palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (palindromString) {
 
+    reversedString = '';
+
+    palindromNoPunc = palindromString
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'');
+
+    palindromNoPuncNoSpace = palindromNoPunc
+        .replace(/\s/g,'');
+
+    palindromNoPuncNoSpaceLC = palindromNoPuncNoSpace.toLowerCase();
+
+    lengthReducedPalindrom = palindromNoPuncNoSpaceLC.length;
+
+    for(i = lengthReducedPalindrom - 1; i >= 0; i--) {
+        reversedString += palindromNoPuncNoSpaceLC[i];
+    }
+
+    return reversedString === palindromNoPuncNoSpaceLC;
+
+
+    // processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    // return (
+    //   processedString
+    //     .split("")
+    //     .reverse()
+    //     .join("") == processedString
+    // );
 };
 
 // Do not edit below this line
