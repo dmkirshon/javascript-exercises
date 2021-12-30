f93a9ff3602a4b947f15bc4cc790b3a7355092e9
dmkirshon <kirshon.david@gmail.com>
2021-12-30 11:36:23 +0200

    feat: Implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        let lastKnownLivingA = a.yearOfDeath;
+        let lastKnownLivingB = b.yearOfDeath;
+        
+        if (!lastKnownLivingA) {
+            lastKnownLivingA = (new Date()).getFullYear();
+        }
+        if (!lastKnownLivingB) {
+            lastKnownLivingB = (new Date()).getFullYear();
+        }
 
+        const lastPerson = lastKnownLivingA - a.yearOfBirth;
+        const nextPerson = lastKnownLivingB - b.yearOfBirth;
+
+        return lastPerson > nextPerson ? -1:1;
+    })[0];
 };
 
 // Do not edit below this line
